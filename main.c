a217806f235220eebf807ba3784f739e194742f2
donberke <berke.official@gmail.com>
2017-11-22 11:41:36 +0100

    projekt alapja kész
    az egyenlőre szükséges file-ok a motor vezérléséhez, kód még nincs

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
+#include "servolib.h"
+
 
 int main(void)
 {
 
     while(1)
     {
+
     }
 }
--- servolib.c
+++ servolib.c
@@ -1,4 +1,7 @@
 
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
 #include "servolib.h"
 
 

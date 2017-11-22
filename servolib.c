1d3e5e7457e020fdc14d4edbef095e744bf110e8
donberke <berke.official@gmail.com>
2017-11-22 17:12:16 +0100

    GPIO init
    GPIO inicializálás, GPIOD 10-15 pinek, első 3 TIM3, utolsó 3 TIM4

--- servolib.c
+++ servolib.c
@@ -2,6 +2,28 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_tim.h"
+#include "stm32f4xx_rcc.h"
 #include "servolib.h"
 
 
+void initPins(void) {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource10, GPIO_AF_TIM3);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource11, GPIO_AF_TIM3);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM3);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource14, GPIO_AF_TIM4);
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource15, GPIO_AF_TIM4);
+
+	GPIO_InitTypeDef initStruct;
+	initStruct.GPIO_Mode = GPIO_Mode_OUT;
+	initStruct.GPIO_OType = GPIO_OType_PP;
+	initStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	initStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	initStruct.GPIO_Pin =
+			GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_Init(GPIOD, &initStruct);
+
+}
